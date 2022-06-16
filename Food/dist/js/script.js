54f292604b2547012b58de9ac84c8c10751785ef
1serj1 <103180265+1serj1@users.noreply.github.com>
2022-06-16 14:00:29 +0500

    1

--- Food/dist/js/script.js
+++ Food/dist/js/script.js
@@ -5,32 +5,31 @@ window.addEventListener(`DOMContentLoaded`,() =>{
               tabsParent = document.querySelector(`.tabheader__items`);  // определяем переменные из HTML
          
         function hideTabContent()  {
-            tabsContent.forEach(item => {
-                  // item.style.display = `none`;  // скрыли элементы tabsContent  - bpyfxfkmyj
-                  item.classList.add(`hide`);
+            tabsContent.forEach(item => {  // перебираем элементы в объекте   tabsContent = document.querySelectorAll(`.tabcontent`),
+                  // item.style.display = `none`;  //  исключили элементы tabsContent  
+                  item.classList.add(`hide`); // добавили класс элементов hide - удалено (из выдачи браузера)
                   item.classList.remove(`show`); // один класс добавляем, другой - удаляем
                   // toggle  не добавляется, что бы не было путаницы
             });
             tabs.forEach(item => {
-                item.classList.remove(`tabheader__item_active`);  // удалили элементы tabsContent
+                item.classList.remove(`tabheader__item_active`);  // удалили элемент `tabheader__item_active` в  tabsContent
          });
         }  
-        
+        // как итог убрали элементы с классом CSS .hide
         function showTabContent(i = 0)  { // задаем по умолчанию запуск с 1 элемента
              // tabsContent [i].style.display = ``;  // показали элементы tabsContent - первоначальный вариант
-             tabsContent [i].classList.add(`show`);
-             tabsContent [i].classList.remove(`hide`);
-             tabs[i].classList.add(`tabheader__item_active`);  // дописали элементы tabsContent
+             tabsContent [i].classList.add(`show`); // в итом элемете массива tabsContent = document.querySelectorAll(`.tabcontent`), добавляем класс show
+             tabsContent [i].classList.remove(`hide`);// в итом элемете массива tabsContent = document.querySelectorAll(`.tabcontent`), удаляем класс hide
+             tabs[i].classList.add(`tabheader__item_active`);  // дописали элементы tabsContent в итый элемент
            }
   
            hideTabContent();
            showTabContent(); // вызов фуий
-           //console.log(  item.classList.add(`hide`)); // вызов фуий
-           
+         
            tabsParent.addEventListener(`click`, (e) => { // обработчик события 
                       const target = e.target;  // техн. переменная, что бы не писать e.target порстоянно
-                      if (target && target.classList.contains(`tabheader__item`)) { // если цель имеет tabheader__item
-                            tabs.forEach((item,i) =>{  // перебираем массив элементов
+                      if (target && target.classList.contains(`tabheader__item`)) { // если цель клика  имеет tabheader__item
+                            tabs.forEach((item,i) =>{  // перебираем массив элементов числом i
                                    if (target == item){  // цель - это элемент
                                           hideTabContent();
                                           showTabContent(i);   // запускаем фуии, при этом на эл-те i  запускаем показ
